a77a70ac27094790f0dc4fb4cdc346a4728418d0
Raphonkzy <raffaustaabyasa@gmail.com>
2023-11-24 07:59:27 +0700

    init

---
+++ src/views/Login.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+import { useAuthStore } from "@/stores/auth"
+import { ref } from 'vue';
+
+const authStore = useAuthStore()
+const router = useRouter()
+
+const username = ref("")
+const password = ref("")
+
+const usernameValid = "raffa"
+const passwordValid = "1234"
+
+const login = (e) => {
+    e.preventDefault()
+    if (username.value == usernameValid && password.value == passwordValid) {
+        console.log(username.value)
+        console.log(password.value)
+
+        authStore.username = username.value
+        authStore.password = password.value
+
+        router.push("/")
+    }
+}
+</script>
+
+<template>
+    <form @submit="(e) => login(e)">
+        <div>
+            <input type="text" placeholder="Username" v-model="username">
+        </div>
+        <div>
+            <input type="text" placeholder="Password" v-model="password">
+        </div>
+        <button type="submit" class="login">Login</button>
+    </form>
+</template>
